a9c0731f278fc5134266aca43885b19f6d8becb8
hp <55326140+Alinawaz-786@users.noreply.github.com>
2021-10-28 10:21:14 +0500

    test

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -16,6 +16,9 @@ async function main() {
 //    if constructor have parameter 
 //    const greeter = await Pizza.deploy("0x1e3d62dcd61cbd86fd682028b0f76cfcc8388898","0x1947d91fb44093a3a0dc06c418afd31742240f38");    
 
+
+
+    
 }
 
 main()
